aa37cd607fb6ec76cc89ee323067e6ba5aaa1c8d
sgarcade <davidsant2188@gmail.com>
2023-08-29 21:29:20 -0500

    Version2

--- hola.py
+++ hola.py
@@ -1,4 +1,8 @@
-    
-def say_hello(name="Worldd"):
- print("Hello ", name, "!")
-say_hello()
+import argparse
+def say_hello(name="World"):
+    print("Hello ", name, "!")
+parser = argparse.ArgumentParser(description='Say hello.')
+parser.add_argument('--name', type=str, required=False,
+metavar='name', help='your name', default='World')
+args = parser.parse_args()
+say_hello(args.name)
