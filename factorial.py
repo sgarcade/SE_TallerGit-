1de11bc2bd91d13802ea2dc7b8ad830ef1b165e1
sgarcade <davidsant2188@gmail.com>
2023-08-29 22:14:51 -0500

    Factorial

---
+++ factorial.py
@@ -0,0 +1,13 @@
+Numero = int(input("Digita un numero: "))
+print(Numero)
+ 
+factorial = 1
+
+for i in range(1,Numero + 1):
+    factorial = factorial * i
+
+
+print("El factorial del numero ", Numero, "es: ", factorial)
+
+        
+
--- hola.py
+++ hola.py
@@ -6,3 +6,4 @@ parser.add_argument('--name', type=str, required=False,
 metavar='name', help='your name', default='World')
 args = parser.parse_args()
 say_hello(args.name)
+    
